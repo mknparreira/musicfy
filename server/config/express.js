246a42a638291724a425cfa2c1215c0a796f41ab
mbenevides <maikon.benevides@lasa.com.br>
2017-01-13 16:59:19 -0200

    Incluindo nodeJs e ExpressJs

--- server/config/express.js
+++ server/config/express.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser'); //Why?
 var routes = require('../core/routes');
+var path = require('path');
 
-app.use(express.static('./public')); //Why?
+app.use(express.static('public'));
+app.use(express.static('application'));
 app.use(bodyParser.urlencoded({extended: true})); //Why?
 
 app.use(bodyParser.json()); //Why?
--- server/core/routes/index.js
+++ server/core/routes/index.js
@@ -2,7 +2,12 @@ var api = require('../api');
 var path = require('path');
 
 module.exports = function(app){
-    app.get('/', function(req, response){
-        response.sendFile(path.resolve('application/public/index.html'));
+    app.get('/', function(req, res){
+        res.sendFile(path.resolve('application/index.html'));
     });
+
+    // app.use('*', function(req, res) {
+    //     res.sendfile(path.resolve('application/lib/angular/angular.js'));
+    //     res.sendfile(path.resolve('application/lib/bootstrap/bootstrap.css'));
+    // });
 };
